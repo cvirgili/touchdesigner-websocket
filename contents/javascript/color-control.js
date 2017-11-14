f9b98c75b9ba2f6709e857be612761ca61f3131c
cvirgili <cristiano.virgili@gmail.com>
2017-11-14 18:30:09 +0100

    ok

--- contents/javascript/color-control.js
+++ contents/javascript/color-control.js
@@ -13,3 +13,24 @@ socket.on('color', function(color) {
     console.log(color);
     document.body.style.backgroundColor = color;
 });
+
+const osc = new OSC()
+
+osc.on('/param/density', (message) => {
+    console.log(message.args)
+})
+
+osc.on(['param', 'volume'], (message) => {
+    console.log(message.args)
+})
+
+osc.on('open', () => {
+    const message = new OSC.Message('/test', 12.221, 'hello')
+    osc.send(message)
+
+    const bundle = new OSC.Bundle(Date.now() + 5000)
+    bundle.add(message)
+    osc.send(bundle, { host: '192.168.178.5' })
+})
+
+osc.open({ port: 9000 })
