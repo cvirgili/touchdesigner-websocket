6d5d537f3622d75c2f19ea4ccdf1650fd134f19d
cvirgili <cristiano.virgili@gmail.com>
2018-05-10 19:39:22 +0200

    ok

--- contents/javascript/color-control.js
+++ contents/javascript/color-control.js
@@ -1,36 +1,49 @@
  // jshint esversion:6
  var c, val, fg, bg, div, vals = [],
-     nitem;
+     nitem, strobeval = 0;
 
  var rows = 10,
      cols = 10;
 
  $.get('/default', function(data) {
-     console.log('color: ' + data.color);
-     console.log('nitem: ' + data.nitem);
      getNItems(data.nitem);
      fg = data.color.substr(0, 7);
      document.getElementById('overlay').style.backgroundColor = fg;
+ }).fail(function() {
+     onDisconnect();
  });
 
  window.addEventListener('load', function() {
      createItems();
-     doIt();
+     setColor();
 
      //socket.forceNew = true;
 
      socket.on('connect', function() {});
-     socket.on('error', function() { socket.close(); });
-     socket.on('disconnect', function() { socket.close(); });
-     socket.on('close', function() { socket.close(); });
+     socket.on('error', function() {
+         onDisconnect();
+         socket.close();
+     });
+     socket.on('disconnect', function() {
+         onDisconnect();
+         socket.close();
+     });
+     socket.on('close', function() {
+         onDisconnect();
+         socket.close();
+     });
      socket.on('color', function(color) {
          fg = color.substr(0, 7);
-         doIt();
+         setColor();
      });
 
+     socket.on('reload', reload);
+
+
      socket.on('strobe', function(strb) {
-         document.getElementById('overlay').style.backgroundColor = (parseFloat(strb) < 0.20) ? fg : "#ffffff";
-         document.getElementById('overlay').style.opacity = parseFloat(strb);
+         strobeval = parseFloat(strb);
+         document.getElementById('overlay').style.backgroundColor = (strobeval >= 0.60) ? "#ffffff" : fg;
+         document.getElementById('overlay').style.opacity = strobeval;
      });
 
      socket.on('nitem', function(n) {
@@ -44,13 +57,26 @@
              document.getElementsByClassName('color').item(i).style.opacity = parseFloat(vals[nitem - i - 1]);
          }
      });
+
+
  });
+
+ function onDisconnect() {
+     document.getElementById('container').innerHTML = "<h1>Bye Bye!</h1><button id='reloadbtn'>RELOAD</button>";
+     $('#reloadbtn').on('click touchstart', reload);
+     document.body.style.backgroundColor = "red";
+ }
+
+
+
+ function reload() {
+     location.reload(true);
+ }
 
  function getNItems(n) {
      rows = parseInt(n.split('|')[0]);
      cols = parseInt(n.split('|')[1]);
      nitem = rows * cols;
-
  }
 
  function createItems() {
@@ -66,7 +92,7 @@
      }
  }
 
- function doIt() {
+ function setColor() {
      for (var i = 0; i < nitem; i++) {
          document.getElementsByClassName('color').item(i).style.backgroundColor = fg;
      }
