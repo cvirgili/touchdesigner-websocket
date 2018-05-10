6fc8504e2c374883ddb1c91972efc0e9b77abc56
cvirgili <cristiano.virgili@gmail.com>
2018-05-10 17:16:55 +0200

    gooood

--- contents/javascript/color-control.js
+++ contents/javascript/color-control.js
@@ -1,9 +1,6 @@
  // jshint esversion:6
- var c, val, fg, bg, div, vals = new Array(),
-     nitem,
-     ampl = 0,
-     rot = 0,
-     count = 0;
+ var c, val, fg, bg, div, vals = [],
+     nitem;
 
  var rows = 10,
      cols = 10;
@@ -11,54 +8,50 @@
  $.get('/default', function(data) {
      console.log('color: ' + data.color);
      console.log('nitem: ' + data.nitem);
-     nitem = parseInt(data.nitem);
+     getNItems(data.nitem);
      fg = data.color + "";
-     createItems();
-     doIt();
      document.getElementById('overlay').style.backgroundColor = fg;
+ });
 
+ socket.forceNew = true;
+
+ socket.on('connect', function() {});
+ socket.on('error', function() { socket.close(); });
+ socket.on('disconnect', function() { socket.close(); });
+ socket.on('close', function() { socket.close(); });
+ socket.on('color', function(color) {
+     fg = color.substr(0, 7);
+     doIt();
+ });
+
+ socket.on('strobe', function(strb) {
+     document.getElementById('overlay').style.backgroundColor = (parseFloat(strb) < 0.20) ? fg : "#ffffff";
+     document.getElementById('overlay').style.opacity = parseFloat(strb);
+ });
+
+ socket.on('nitem', function(n) {
+     getNItems(n);
+     createItems();
+ });
+
+ socket.on('values', function(val) {
+     vals = val.split('\n').join('').split('|');
+     for (var i = nitem - 1; i >= 0; i--) {
+         document.getElementsByClassName('color').item(i).style.opacity = parseFloat(vals[nitem - i - 1]);
+     }
  });
 
  window.addEventListener('load', function() {
-
-     socket.forceNew = true;
-
      createItems();
-
-     socket.on('connect', function() {
-         console.log('connected');
-         this.emit('Hello!');
-
-     });
-
-     socket.on('error', function() { socket.close(); });
-     socket.on('disconnect', function() { socket.close(); });
-     socket.on('close', function() { socket.close(); });
-     socket.on('color', function(color) {
-         fg = color.substr(0, 7);
-         doIt();
-     });
-
-     socket.on('strobe', function(strb) {
-         document.getElementById('overlay').style.backgroundColor = (parseFloat(strb) < 0.20) ? fg : "#fff";
-         document.getElementById('overlay').style.opacity = parseFloat(strb);
-     });
-     socket.on('nitem', function(n) {
-         rows = parseInt(n.split('|')[0]);
-         cols = parseInt(n.split('|')[1]);
-         nitem = rows * cols;
-         createItems();
-     });
-     socket.on('values', function(val) {
-         vals = [];
-         vals = val.split('\n').join('').split('|');
-         for (var i = nitem - 1; i >= 0; i--) {
-             document.getElementsByClassName('color').item(i).style.opacity = parseFloat(vals[nitem - i - 1]);
-         }
-     });
-
+     doIt();
  });
 
+ function getNItems(n) {
+     rows = parseInt(n.split('|')[0]);
+     cols = parseInt(n.split('|')[1]);
+     nitem = rows * cols;
+
+ }
 
  function createItems() {
      document.getElementById('container').innerHTML = "";
@@ -71,7 +64,6 @@
          div.style.backgroundColor = fg;
          document.getElementById('container').appendChild(div);
      }
-
  }
 
  function doIt() {
